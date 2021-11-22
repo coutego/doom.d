7ae88ea37fc578663e84d4b9412a6552885e95b9
Coutego <coutego@gmail.com>
2021-11-22 22:53:02 +0100

    move function to change split direction to ctg-win

--- config.el
+++ config.el
@@ -73,35 +73,6 @@
 (setq doom-localleader-alt-key "C-,")
 (setq doom-leader-alt-key "C-SPC")
 
-;; Function to change window split orientation from vertical to horizontal
-;; and viceversa
-(defun toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer)))
-       (next-win-buffer (window-buffer (next-window)))
-       (this-win-edges (window-edges (selected-window)))
-       (next-win-edges (window-edges (next-window)))
-       (this-win-2nd (not (and (<= (car this-win-edges))))
-           (car next-win-edges
-             (<= (cadr this-win-edges)))
-           (cadr next-win-edges))
-       (splitter
-        (if (= (car this-win-edges))
-         (car (window-edges (next-window))))))
-      'split-window-horizontally
-    'split-window-vertically)
-  (delete-other-windows)
-  (let ((first-win (selected-window)))
-    (funcall splitter)
-    (if this-win-2nd (other-window 1))
-    (set-window-buffer (selected-window) this-win-buffer)
-    (set-window-buffer (next-window) next-win-buffer)
-    (select-window first-win)
-    (if this-win-2nd (other-window 1))))
-
-(define-key ctl-x-4-map "t" 'toggle-window-split)
-
 ;; Avy configuration
 (setq avy-all-windows t)
 
@@ -110,7 +81,7 @@
       :desc "Goto word" "j w" #'avy-goto-word-1)
 
 (map! :leader
-      :desc "Toggle window split" "j s" #'toggle-window-split)
+      :desc "Toggle window split" "j s" #'ctg-toggle-window-split)
 
 (map! :leader
       :desc "Change window" "w w" #'ace-window)

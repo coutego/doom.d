01acd9aae66173331134232d69c8d2715b2055fc
Pedro Abelleira Seco <pedroabelleira@gmail.com>
2021-12-05 12:06:00 +0100

    fix parinfer error on lisp buffer load

--- config.el
+++ config.el
@@ -223,12 +223,12 @@
              paredit        ; Introduce some paredit commands.
              smart-tab      ; C-b & C-f jump pos and smart shift with tab & S-tab.
              smart-yank))   ; Yank behavior depend on mode.
-    (setq parinfer-auto-switch-indent-mode t)
-    (add-hook 'clojure-mode-hook #'parinfer-mode)
-    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-    (add-hook 'scheme-mode-hook #'parinfer-mode)
-    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+    (setq parinfer-auto-switch-indent-mode t)))
+    ;; (add-hook 'clojure-mode-hook #'parinfer-mode)
+    ;; (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    ;; (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    ;; (add-hook 'scheme-mode-hook #'parinfer-mode)
+    ;; (add-hook 'lisp-mode-hook #'parinfer-mode)))
 
 (use-package! ctg-win
   :after evil

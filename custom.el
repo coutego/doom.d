20e060d1e443cb976a4420d6cbc549e9de130953
Coutego <coutego@gmail.com>
2022-04-17 11:17:02 +0200

    Fix PATH when running from dock

--- custom.el
+++ custom.el
@@ -5,7 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(avy-all-windows 'all-frames)
  '(exec-path
-   '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/opt/homebrew/Cellar/emacs-plus@28/28.0.50/libexec/emacs/28.0.90/aarch64-apple-darwin21.1.0" "/opt/homebrew/bin" "/opt/homebrew/sbin" "/usr/local/bin"))
+   '("/Users/pedroabelleiraseco/opt/bin"
+     "/opt/homebrew/bin"
+     "/opt/homebrew/sbin"
+     "/usr/local/bin"
+     "/usr/bin"
+     "/bin"
+     "/usr/sbin"
+     "/sbin"
+     "/Library/TeX/texbin"
+     "/Library/Apple/usr/bin"
+     "/opt/homebrew/Cellar/emacs-plus@28/28.0.50/libexec/emacs/28.0.90/aarch64-apple-darwin21.1.0"))
  '(package-selected-packages '(org-web-tools selectric-mode vertico org-ml web-server))
  '(safe-local-variable-values
    '((elisp-lint-indent-specs
@@ -28,3 +38,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Fix PATH when running from dock
+(setenv "PATH" (s-join ":" exec-path))
